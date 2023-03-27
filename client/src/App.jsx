32fb5df60974bfebf2650978f0a076b73da5bde9
michmoosch <michael.moore01@sjsu.edu>
2023-03-27 13:25:52 -0700

    Connected to OpenAI Api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,10 +9,26 @@ function App() {
   const [data, setData] = useState();
 
   const getData = async () => {
-    console.log("Fetching");
     const msg = await fetch("/api/get");
     const newData = await msg.json();
+    console.log(newData.msg);
     setData((prev) => newData);
+  };
+
+  const postData = async () => {
+    const resp = await fetch("/api/post", {
+      method: "POST",
+      body: JSON.stringify({
+        title: "foo",
+        body: "Write me a haiku about the wind",
+        userId: 1,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+    const msg = await resp.json();
+    setData((prev) => msg);
   };
 
   useEffect(() => {}, []);
@@ -23,6 +39,9 @@ function App() {
         {data}
         <Button onClick={getData} className="btn">
           Get
+        </Button>
+        <Button onClick={postData} className="btn">
+          Post
         </Button>
       </Container>
     </div>
--- server/index.js
+++ server/index.js
@@ -1,13 +1,40 @@
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
+const { Configuration, OpenAIApi } = require("openai");
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/get", (req, res) => {
-  res.json("{'msg': 'NONONOONNO'}");
+const API_KEY = process.env.OPENAI_API;
+const API_ORG = process.env.OPENAI_ORG;
+
+const config = new Configuration({
+  organization: API_ORG,
+  apiKey: API_KEY,
+});
+
+const openai = new OpenAIApi(config);
+
+app.get("/get", async (req, res) => {
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: "Write me a haiku about programming" }],
+  });
+  const text = completion.data.choices[0].message.content;
+
+  res.json(`{'msg': '${text}'}`);
+});
+
+app.post("/post", async (req, res) => {
+  const prompt = req.body.body;
+
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: prompt }],
+  });
+  const text = completion.data.choices[0].message.content;
+  res.json(`{'msg': '${text}'}`);
 });
 
 app.listen("3001", () => {});
